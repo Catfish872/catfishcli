e95672a0371980c2ca1b5f3b3e1599ded2e3d1b8
catfish872 <90752914+Catfish872@users.noreply.github.com>
2025-08-15 07:29:40 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 检查环境变量是否存在，如果存在，就把它写入到文件中
-if [ -n "$GEMINI_CREDENTIALS_JSON" ]; then
-  echo "$GEMINI_CREDENTIALS_JSON" > "$GOOGLE_APPLICATION_CREDENTIALS"
-fi
-
-# 执行 Dockerfile 中原本的 CMD 命令
-exec "$@"
